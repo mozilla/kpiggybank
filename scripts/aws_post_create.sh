18baf066d4c5ad4663983e771fff88c04005ccd7
Katie Parlante <kparlante@mozilla.com>
2012-12-20 13:13:06 -0800

    Added couchdb setup to awsbox config

---
+++ scripts/aws_post_create.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# start up couch db to add users
+sudo service couchdb start
+
+# curl will fail without a delay
+sleep 10
+
+# create an admin user
+HOST="http://localhost:5984"
+curl -X PUT $HOST/_config/admins/admin -d '"admin"'
+
+# create the piggybank user 
+# now that an admin user exists, change the host
+HOST="http://admin:admin@localhost:5984"
+curl -X PUT $HOST/_config/admins/kpiggybank -d '"kpiggybank"'
+
+# create the kpi db
+curl -X PUT $HOST/bid_kpi
